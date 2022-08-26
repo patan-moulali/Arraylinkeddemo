798309445ac58b79aacf01dd5393643040218711
Patan Moulali <patan.moulali@mphasis.com>
2022-08-26 12:01:21 +0530

    mm

---
+++ Arraylinkeddemo/linkedlistdemo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arraylinkeddemo
+{
+    internal class Linkedlistdemo
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> mylist = new LinkedList<int>();
+            mylist.AddFirst(10);
+            mylist.AddLast(100);
+
+
+        }
+    }
+}
