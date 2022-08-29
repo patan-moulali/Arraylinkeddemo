de3e44ef1ec1ad41ae7ac8aa6ea0eac47680d055
Patan Moulali <patan.moulali@mphasis.com>
2022-08-29 16:51:50 +0530

    p3

---
+++ Arraylinkeddemo/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arraylinkeddemo
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            // Creating a list of strings
+            List<string> Product = new List<string>();
+
+            Product.Add("pen");
+            Product.Add("pencil");
+            Product.Add("ink");
+
+
+            System.Console.WriteLine("Original List:");
+
+            Product.ForEach(System.Console.WriteLine);
+
+
+
+            Product.Remove("pencil");
+
+            System.Console.WriteLine();
+
+            System.Console.WriteLine("After removing pencil:");
+
+            Product.ForEach(System.Console.WriteLine);
+        }
+    }
+}
---
+++ Arraylinkeddemo/objnew.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arraylinkeddemo
+{
+    internal class Emplopyee
+    {
+        static void Main(String[] args)
+        {
+            employee emp1 = new Employee();
+
+        }
+    }
+}
